8dc911d2306bc6a42116793b5676c057d73bebaf
Семён Фролов <ohriptut123@mail.ru>
2023-09-03 23:55:31 +0300

    .

--- src/components/PostContainer.tsx
+++ src/components/PostContainer.tsx
@@ -1,13 +1,24 @@
+import { useState } from 'react'
 import { postAPI } from '../services/PostService'
 import PostItem from './PostItem'
 
 const PostContainer = () => {
-    const { data: posts, error, isLoading } = postAPI.useFetchAllPostsQuery(10)
+    const [limit, setLimit] = useState(10)
+    const { data: posts, error, isLoading } = postAPI.useFetchAllPostsQuery(limit)
+
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const value = parseInt(event.target.value);
+        setLimit(value);
+    };
 
     return (
         <div>
             {isLoading && <h1>Идет загрузка...</h1>}
             {error && <h1>Ошибка</h1>}
+
+            <input type="number" value={limit} onChange={handleChange}/>
+
             {posts?.map((post, index) => {
                 return (
                     <PostItem post={post} key={index} />
